2309d01a30cc1abf26ae7d7c8219b02bbccefdae
Icaro Roberto <oi@icaroroberto.com>
2021-02-04 18:18:29 -0300

    Added swagger files

--- lib/quero_api_web/endpoint.ex
+++ lib/quero_api_web/endpoint.ex
@@ -24,7 +24,8 @@ defmodule QueroApiWeb.Endpoint do
     at: "/",
     from: :quero_api,
     gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    # only: ~w(swagger.json),
+    headers: %{"Access-Control-Allow-Origin" => "http://localhost:8888"}
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
